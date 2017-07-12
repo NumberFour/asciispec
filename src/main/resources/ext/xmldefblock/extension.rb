24fba2175c989cb05d51257e17c191efc449e872
bsmith <brian.smith@numberfour.eu>
2017-07-12 11:17:50 +0200

    GH-36 add basic tests for def and req blocks to XML

--- src/main/resources/ext/xmldefblock/extension.rb
+++ src/main/resources/ext/xmldefblock/extension.rb
@@ -8,15 +8,27 @@ class XmlDefBlock < Extensions::BlockProcessor
   on_contexts :open, :paragraph, :example, :listing, :sidebar, :pass
 
   def process parent, reader, attrs
-    # Add pass characters here to prevent the anchor from being processed
-    #    as regular adoc content i.e. html character replacements for < > tags
+    # Add pass characters here to prevent html character replacements for < > tags
     pass = "+++"
     attrs['name'] = 'definition'
     attrs['caption'] = 'Definition: '
-    title = attrs['caption'] + attrs['title']
-    anchor = (create_anchor parent, title, type: :link, target: title).convert
-    endcontent = reader.lines.unshift(pass, anchor, pass)
-    create_block parent, :admonition, endcontent, attrs, content_model: :compound
-  end
+    
+    # downcase the title and replace spaces with underscores.
+    #    Also replacing double quotes with single quotes to stop xml from breaking
+    #    in the case where the title contains a quote symbol
+    formatted_title = attrs['title'].downcase.tr(" ", "_").tr("\"", "'")
 
+    # TODO try and use the built-in behaviour of generating anchors:
+    #    link = (create_anchor parent, attrs['title'], type: :link, target: attrs['title']).render
+    #    not working due to missing 'linkend' property.  
+    link = "<link linkend=\"#{formatted_title}\">#{attrs['title']}</link>"
+    anchor = "<anchor xml:id=\"#{formatted_title}\" xreflabel=\"[#{formatted_title}]\"/>"
+    def_prefix = "<emphasis role=\"strong\">Definition:</emphasis>"
+
+    # concatenate all generated lines and prepend before the included 
+    #   definition block content
+    concat_lines = reader.lines.unshift(pass, anchor, def_prefix, link, pass)
+
+    create_block parent, :admonition, concat_lines, attrs, content_model: :compound 
+  end
 end
